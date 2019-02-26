f816a37f7121dba8914383ece332eb341a37313b
AniDas10 <ani2004@gmail.com>
2019-02-27 02:17:08 +0530

    Content successfully extracted and stored in a text file

---
+++ scrape.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = requests.get('https://redditmetrics.com/top')
+
+soup = BeautifulSoup(url.text, 'html.parser')
+
+#print(soup)
+
+with open('content.txt', 'w') as f:
+    for subreddit in soup.find_all('a'):
+        try:
+
+            if '/r' in subreddit.string:
+                f.write(subreddit.string[3:] + '\n')
+
+        except:
+
+            TypeError
